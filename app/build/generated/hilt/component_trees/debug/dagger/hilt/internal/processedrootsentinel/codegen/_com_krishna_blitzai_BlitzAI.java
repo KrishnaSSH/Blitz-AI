c8cc70fa66f3f8a7cace0f715d042d265b4f0e4f
krishna <Krishna.pytech@gmail.com>
2024-09-11 22:22:13 +0530

    ignore

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_krishna_blitzai_BlitzAI.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.krishna.blitzai.BlitzAI"
-)
-public final class _com_krishna_blitzai_BlitzAI {
-}
--- app/build/tmp/kapt3/stubs/debug/com/krishna/blitzai/repository/base/BaseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.krishna.blitzai.repository.base;
-
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\b&\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/krishna/blitzai/repository/base/BaseRepository;", "", "()V", "app_debug"})
-public abstract class BaseRepository {
-    
-    public BaseRepository() {
-        super();
-    }
-}
